7fb372e093df8defbc47f97ea400b808d0b670c6
Vladimir <acbkaa@gmail.com>
2017-04-26 22:18:31 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 #глобальные переменные
-BOT_TOKEN = "xoxb-174808504294-MdlOsjlVt81k3rBSihVq41Br"
-CHANNEL_NAME = "test_chanel_for_bot"
+BOT_TOKEN =  "token"
+CHANNEL_NAME = "channel"
