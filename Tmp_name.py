219caca1f227c9c84b5d1dc2b41459f7d179e796
Vladimir <acbkaa@gmail.com>
2017-04-26 23:44:36 +0300

    Update Tmp_name.py

--- Tmp_name.py
+++ Tmp_name.py
@@ -2,55 +2,56 @@
 
 import time
 from slackclient import SlackClient
+from bot_config import*
 import google
-from bot_configs import*        #импортируем настройки из bot_config
-import requests
+import datetime
 
 def search(what):
-    for url in google.search(what, lang='eng',stop=5):
-        return (url)
+   for url in google.search(what, lang='ru',stop=5):
+       return (url)
+    
+def what_time(what):
+    now = datetime.datetime.now()
+    moment = now.time()
+    return (str(moment)[:8:])
 
-what = {
-    'погода': "Погода Отличная, я ж загораю!",
-    'жизнь' : "Житуха вапшэ супэр!",
-    'менделева': 'нет. не хочу =('
-}
-you = {
-    'дебил': 'Иди в Жопу!',
-    'молодец': 'Спасибо, мудак!',
-    'lol': 'что смешного???'
+hello_list = ('привет','добрый день','здарова')
+time_list = ('который час','сколько время','время',)
+what_list = ('что такое','кто такой','где найти')
 
-}
-russian = {
-    'норм': 'Я понимаю русский!'
-}
 Questions = {
-    ' what is ': search,
-
+    what_list : search,
+    hello_list: 'Приветствую!\n',
+    time_list: what_time
 
 }
-
 
 if __name__ == '__main__':
-    sc = SlackClient(BOT_TOKEN)
+   sc = SlackClient(BOT_TOKEN)
 
-   # присоеденяемся к слаку
-    if sc.rtm_connect():
-        # шлем приветсвие
-        sc.rtm_send_message(CHANNEL_NAME, "Ready for Work!")
-    while True:
-        # читаем сообщения
-        for slack_message in sc.rtm_read():
-            message = slack_message.get("text")
-            print message
-            test = 'bender'
-            user = slack_message.get("user")
-            if message == None or user == None:
-                continue
-            elif test in message.lower():
-                for key,values in Questions.items():
-                    if key in message.lower():
-                        message_to_user = values(message[6::])
-                        sc.rtm_send_message(CHANNEL_NAME, ("<@{}> "+message_to_user).format(user))
+ # присоеденяемся к слаку
+   if sc.rtm_connect():
+       # шлем приветсвие
+       sc.rtm_send_message(CHANNEL_NAME, "Ready for Work!")
+   while True:
+       # читаем сообщения
+       for slack_message in sc.rtm_read():
+           message = slack_message.get("text")
+           test = 'бот'
+           user = slack_message.get("user")
+           if message == None or user == None:
+               continue
+           elif test in message.lower().encode('utf-8'):
+               for key,values in Questions.items():
+                   for vars in key:
+                       if vars in message.lower().encode('utf-8'):
+                           print type(values)
+                           if type(values) == str:
+                               message_to_user = values
+                               sc.rtm_send_message(CHANNEL_NAME, ("<@{}> " + message_to_user).format(user))
+                           else:
+                               message_to_user = values(message[6::].encode('utf-8'))
+                               sc.rtm_send_message(CHANNEL_NAME, ("<@{}> "+message_to_user).format(user))
 
-       # спим неного
+     # спим неного
+       time.sleep(0.1)
