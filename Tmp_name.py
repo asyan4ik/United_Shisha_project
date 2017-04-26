04acaf544fe5c3ba6d8261c798480db3424508c8
Shisha13 <kaktuz.fmc@gmail.com>
2017-04-26 10:49:01 +0300

    Update Tmp_name.py
    Теперь он гуглит

--- Tmp_name.py
+++ Tmp_name.py
@@ -1,30 +1,61 @@
 # -*-coding: utf-8 -*-
+
 import time
 from slackclient import SlackClient
+import google
+import requests
 
-BOT_TOKEN = "token"
-CHANNEL_NAME = "channel"
+def search(what):
+    for url in google.search(what, lang='eng',stop=5):
+        return (url)
 
-def main():
-    sc = SlackClient(BOT_TOKEN)
 
-    # присоеденяемся к слаку
-    if sc.rtm_connect():
-        # шлем приветсвие
-        sc.rtm_send_message(CHANNEL_NAME, "Я снова жив!!!")
 
-        while True:
-            # читаем сообщения 
-            for slack_message in sc.rtm_read():
-                message = slack_message.get("text")
-                user = slack_message.get("user")
-                if not message or not user:
-                    continue
-                sc.rtm_send_message(CHANNEL_NAME, "<@{}> отвали от меня , я ничего не умею еще".format(user))
-            # спим неного
-            time.sleep(0.5)
-    else:
-        print("Что-то пошло не так . не могу подключится")
+BOT_TOKEN = "xoxb-174808504294-MdlOsjlVt81k3rBSihVq41Br"
+CHANNEL_NAME = "test_chanel_for_bot"
+
+what = {
+    'погода': "Погода Отличная, я ж загораю!",
+    'жизнь' : "Житуха вапшэ супэр!",
+    'менделева': 'нет. не хочу =('
+}
+you = {
+    'дебил': 'Иди в Жопу!',
+    'молодец': 'Спасибо, мудак!',
+    'lol': 'что смешного???'
+
+}
+russian = {
+    'норм': 'Я понимаю русский!'
+}
+Questions = {
+    ' what is ': search,
+
+
+}
+
 
 if __name__ == '__main__':
-    main()
+    sc = SlackClient(BOT_TOKEN)
+
+   # присоеденяемся к слаку
+    if sc.rtm_connect():
+        # шлем приветсвие
+        sc.rtm_send_message(CHANNEL_NAME, "Ready for Work!")
+    while True:
+        # читаем сообщения
+        for slack_message in sc.rtm_read():
+            message = slack_message.get("text")
+            print message
+            test = 'bender'
+            user = slack_message.get("user")
+            if message == None or user == None:
+                continue
+            elif test in message.lower():
+                for key,values in Questions.items():
+                    if key in message.lower():
+                        message_to_user = values(message[6::])
+                        sc.rtm_send_message(CHANNEL_NAME, ("<@{}> "+message_to_user).format(user))
+
+       # спим неного
+        time.sleep(0.5)
