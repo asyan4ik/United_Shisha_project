9e6e6ac2e7a7d3c0cadecf36071416e212aadd25
Vladimir <acbkaa@gmail.com>
2017-04-27 12:57:07 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -3,12 +3,3 @@
 BOT_TOKEN =  "token"
 CHANNEL_NAME = "channel"
 CHAT_BOT_NAME = "бот"   #бывший test='бот'
-
-def search(what):
-   for url in google.search(what, lang='ru',stop=5):
-       return (url)
-
-def what_time(what):
-    now = datetime.datetime.now()
-    moment = now.time()
-    return (str(moment)[:8:])
